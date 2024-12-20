045f3ef8f7973fbed6dd2f99b8250423e40e3568
Hm Elius Hossain Himel <himelahmed201617@gmail.com>
2024-12-20 21:28:13 +0600

    fix: update Rollup configuration to use 'src/index.ts' as entry file and add TypeScript plugin

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,20 +2,22 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import url from "@rollup/plugin-url";
+import typescript from "@rollup/plugin-typescript";
 
 export default {
-  input: "index.ts",
+  input: "src/index.ts", // Your entry file
   output: {
     file: "dist/index.js",
-    format: "esm",
+    format: "esm", // ES Module format
   },
   plugins: [
     resolve(),
     commonjs(),
     json(),
     url({
-      include: ["**/*.svg"], // Include only SVG files
-      limit: 0, // Disable inlining, export file paths instead
+      include: ["**/*.svg"],
+      limit: 0, // Export SVG file paths
     }),
+    typescript(),
   ],
 };
